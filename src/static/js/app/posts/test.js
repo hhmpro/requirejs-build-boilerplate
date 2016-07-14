1edc7505a8eedc7d7c62fdccb58b2d82faaad5a0
hhmpro <240050497@qq.com>
2016-07-15 02:18:44 +0800

    监听修改自动更新，不加版本号和加版本号构建

---
+++ src/static/js/app/posts/test.js
@@ -0,0 +1,6 @@
+require(['zepto', 'juicer','util'], function($, juicer, util) {
+    console.log("zepto:", $);
+    console.log("juicer:", juicer);
+    util.say();
+    console.log("it works!");
+});
